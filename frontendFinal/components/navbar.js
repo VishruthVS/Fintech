f6b164bb06a10748cb4dd3b5bc3b0e3670c37545
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-03-09 03:42:38 +0530

    last commit

--- frontendFinal/components/navbar.js
+++ frontendFinal/components/navbar.js
@@ -72,7 +72,7 @@ const Navbar = () => {
                 </li>
                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
                   <Link
-                    href="https://dao.matcrypt.com/"
+                    href="https://matcrypt.com/"
                     onClick={() => setNavbar(!navbar)}
                   >
                     {" "}
